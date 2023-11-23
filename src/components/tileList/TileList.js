68f903e9a1e9f978999529fb66676e8033984937
Omar ♔ <o_yousaf@live.co.uk>
2023-11-23 18:20:52 +0000

    return empty object if description is null

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,13 +1,16 @@
 import React from "react";
-
 import Tile from "../tile/Tile";
 
 export const TileList = ({ data }) => {
   return (
     <div>
-      {data.map((item, index) => {
-        <Tile key={index} name={item.name} description={item.description} />;
-      })}
+      {data.map((item, index) => (
+        <Tile
+          key={index}
+          name={item.name}
+          description={item.description || {}}
+        />
+      ))}
     </div>
   );
 };
